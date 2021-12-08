537e8a799ecd91994d2cbc6e4ad50ab5931b208f
Yusuke Izawa <me@yizawa.com>
2021-12-08 20:30:28 +0900

    moved.

--- test/simple.ml
+++ test/simple.ml
@@ -1,8 +1,7 @@
 let%mj rec f x =
-  if x < 0 then 1024 else 2048
+  if 0 < x then 2048 else 1024
 in
 let%tj rec g y =
   let z = f y in
-  if y < 1 then y + z else
-    g (y-1)
+  if 1 < y then g (y-1) else y + z
 in print_int (g 10)
