eab041a695cae33947d19db52dbebfe90484eb5c
Yusuke Izawa <me@yizawa.com>
2022-03-31 20:58:54 +0900

    update

--- test/fact.ml
+++ test/fact.ml
@@ -1,5 +1,5 @@
 let rec fact x =
   if x <= 1 then 1 else
     x * fact (x-1) in
-let r = fact 1850 in
+let r = fact 4000 in
 print_int r
--- test/sieve.ml
+++ test/sieve.ml
@@ -1,10 +1,10 @@
 let rec loop1 flags k i =
-  if k <= 8191 then
+  if k <= 8195 then
     (flags.(k) <- 1;
      loop1 flags (k+i) i)
   else flags in
 let rec sieve flags i =
-  if i <= 8191 then
+  if i <= 8195 then
     let x = flags.(i) in
     if x > 1 then
       let flags = loop1 flags (i+i) i in
@@ -13,7 +13,7 @@ let rec sieve flags i =
       sieve flags (i+1)
   else
     flags in
-let flags = Array.make 8192 2 in
+let flags = Array.make 8196 2 in
 flags.(0) <- 1; flags.(1) <- 1;
 let _ = sieve flags 2 in
 print_int (flags.(14));
