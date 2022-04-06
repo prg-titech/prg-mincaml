64ce76665904c60355832d85f4cea5d94a161830
Yusuke Izawa <me@yizawa.com>
2022-04-07 00:58:04 +0900

    fix sieve

--- test/sieve.ml
+++ test/sieve.ml
@@ -1,21 +1,19 @@
-let rec loop1 flags k i n =
-  if n-1 < k then flags
-  else
+let rec loop1 flags k i =
+  if k <= 131135 then
     (flags.(k) <- 1;
-    loop1 flags (k+i) i n)
-in
-let rec sieve flags i n =
-  if n - 1 < i then flags
-  else
+     loop1 flags (k+i) i)
+  else flags in
+let rec sieve flags i =
+  if i <= 131135 then
     let x = flags.(i) in
-    if x < 2 then
-      sieve flags (i+1) n
+    if x >= 2 then
+      let flags = loop1 flags (i+i) i in
+      sieve flags (i+1)
     else
-      let flags = loop1 flags (i+i) i n in
-      sieve flags (i+1) n
-in
-let n = 131136 in
-let flags = Array.make n 2 in
-flags.(0) <- 1; flags.(1) <- 1; flags.(2) <- 1;
-let _ = sieve flags 3 n in
-print_int (flags.(20))
+      sieve flags (i+1)
+  else
+    flags in
+let flags = Array.make 131136 2 in
+flags.(0) <- 1; flags.(1) <- 1;
+let _ = sieve flags 2 in
+print_int (flags.(14))
